c731c7adaec6dcd729164bcebde0df788f6b8476
aerobounce <10491362+aerobounce@users.noreply.github.com>
2019-12-28 13:07:15 +0900

    initial commit

---
+++ Casks/swiftformat-for-xcode.rb
@@ -0,0 +1,19 @@
+cask "swiftformat-for-xcode" do
+  name "SwiftFormat for Xcode"
+  version "0.43.2"
+  sha256 :no_check
+  url "https://github.com/nicklockwood/SwiftFormat/archive/#{version}.zip"
+  appcast "https://github.com/nicklockwood/SwiftFormat/releases.atom"
+  homepage "https://github.com/nicklockwood/SwiftFormat"
+
+  depends_on macos: ">= :sierra"
+
+  app "SwiftFormat-#{version}/EditorExtension/SwiftFormat for Xcode.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/com.charcoaldesign.SwiftFormat-for-Xcode",
+    "~/Library/Application Scripts/com.charcoaldesign.SwiftFormat-for-Xcode.SourceEditorExtension",
+    "~/Library/Containers/com.charcoaldesign.SwiftFormat-for-Xcode",
+    "~/Library/Group Containers/com.charcoaldesign.SwiftFormat",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,8 @@
+# Cask for SwiftFormat for Xcode.app
+Cask for the app of [nicklockwood/SwiftFormat](https://github.com/nicklockwood/SwiftFormat).
+
+
+# Install
+```
+brew cask install aerobounce/swiftformat-for-xcode/swiftformat-for-xcode
+```
