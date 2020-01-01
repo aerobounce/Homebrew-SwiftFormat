dce7cf13dc3f22b0988e0b54ad412cb5b7406f86
aerobounce <10491362+aerobounce@users.noreply.github.com>
2020-01-01 12:21:49 +0900

    auto update

--- updater.sh
+++ updater.sh
@@ -9,13 +9,14 @@ caskVersion=$(
 )
 
 latestVersion=$(
-    curl https://github.com/nicklockwood/SwiftFormat/releases.atom 2> /dev/null |
+    curl https://github.com/nicklockwood/SwiftFormat/releases.atom |
         grep -m 1 -E "<title>([0-9]{1}.[0-9]{2}.[0-9]{1,2})" |
         sed -e 's/    <title>//g' -e 's/<\/title>//g'
 )
 
 if [[ $caskVersion != "$latestVersion" ]]; then
-    sed -i -e "s/^  version.*/  version \"$latestVersion\"/" ./Casks/swiftformat-for-xcode.rb
+    sed -i '' -e "s/^  version.*/  version \"$latestVersion\"/" ./Casks/swiftformat-for-xcode.rb
 fi
 
+wait
 git add . && git commit -m "auto update" && git push origin
