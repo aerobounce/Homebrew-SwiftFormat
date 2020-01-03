46cf28389417208486558d38f98a9b2ed086da13
aerobounce <10491362+aerobounce@users.noreply.github.com>
2020-01-03 13:45:58 +0900

    auto commit by updater.sh

--- Casks/swiftformat-for-xcode.rb
+++ Casks/swiftformat-for-xcode.rb
@@ -1,6 +1,6 @@
 cask "swiftformat-for-xcode" do
   name "SwiftFormat for Xcode"
-  version "0.43.3"
+  version "0.43.4"
   sha256 :no_check
   url "https://github.com/nicklockwood/SwiftFormat/archive/#{version}.zip"
   appcast "https://github.com/nicklockwood/SwiftFormat/releases.atom"
